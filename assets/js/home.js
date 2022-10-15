60a033f745742b0e699a6af39498e5862ba5f31a
Priscila Passos <cylla_@hotmail.com>
2022-10-15 20:06:06 -0300

    atualização counter

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,9 @@
+let btrefresh = document.querySelector("#refresh");
+let btContinuar = document.querySelector("#continuar");
+let btLogout = document.querySelector("#logout");
+let reiniciar;
+
+
 function mostrarHora(){
     let tempo = new Date();
     let hora = tempo.getHours();
@@ -33,3 +39,37 @@ function mostrarData(){
 
     document.getElementById("data").innerHTML = dia+", "+ data+" de "+mes+" de "+ano;
 }
+
+
+function counter(){
+    let  counter = 600
+      
+       reiniciar = setInterval(() => {
+            counter--;
+            document.getElementById("counter").innerHTML = counter;
+
+            if (counter == 0){
+                clearTimeout(counter);
+                window.location.href = "/../index.html";
+            }
+           
+        }, 1000);
+}
+
+btrefresh.addEventListener("click", function(){
+    clearInterval(reiniciar);
+    counter();
+})
+
+btLogout.addEventListener("click", function(){
+    var resultado = confirm("Deseja realmente Sair ?");
+        if (resultado == true) {
+            localStorage.clear();
+            window.location.assign("/index.html");   
+        }
+        
+})
+
+btContinuar.addEventListener("click", function(){
+    window.location.href = "https://www.compass.uol/";
+})
