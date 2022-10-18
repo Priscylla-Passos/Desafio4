a24a8071eeaed87e8584e4652cdd2aac6099cba3
Priscila Passos <cylla_@hotmail.com>
2022-10-18 09:52:53 -0300

    Final - Entrega

--- assets/js/login.js
+++ assets/js/login.js
@@ -32,16 +32,39 @@ function atualizar(){
 
 //Eventos
 btContinuar.addEventListener("click", function(){
-       
-    if(usuario.value != "" && senha.value != ""){
-        acesso = {
-            usuario: usuario.value,
-            senha: senha.value,
-        }
+    let recuperar = localStorage.getItem(`acesso`);
+    let recuperado = JSON.parse(recuperar)
 
-        localStorage.setItem("acesso", JSON.stringify(acesso));
-                     
-        window.location.href = "../assets/pages/home.html";
+    console.log(recuperado)
+    // console.log(recuperado.usuario)
+    // console.log(recuperado.usuario == usuario.value)
+    // console.log(recuperado.senha == senha.value)
+    console.log(recuperado == null)
+
+    if(recuperado == null){
+        document.getElementById("erro").style.visibility = "visible";
+        document.getElementById("input_usuario").style.border = "1px solid #E9B425"
+        document.getElementById("input_senha").style.border = "1px solid #E9B425"
+    }
+
+    if(usuario.value != "" && senha.value != ""){
+        if (recuperado == null){
+      
+            acesso = {
+                usuario: usuario.value,
+                senha: senha.value,
+            }
+    
+            localStorage.setItem("acesso", JSON.stringify(acesso));
+                         
+            window.location.href = "../assets/pages/home.html";
+
+        }
+        if (recuperado.usuario == usuario.value && recuperado.senha == senha.value){
+            
+                window.location.href = "../assets/pages/home.html";
+
+        } 
 
     }else{
         document.getElementById("erro").style.visibility = "visible";
